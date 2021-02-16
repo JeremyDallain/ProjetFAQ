7ca8ac464e35df60604816493cdce0dfa1744d8f
AZEVsyl86 <azevedosylvain@gmail.com>
2021-02-16 12:39:41 +0100

    commit

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <?php
+    $titre="Admin";
+    require_once "header.php";
+    ?>
+    <main>
+            
+    </main>
+    <?php
+    require_once "footer.php";
+    ?>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <title> Hackathon FAQ : <?php echo $titre; ?></title>
+    <link rel="icon" type="image/png" href="img/"/>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="styles/style.css"/>
+    <meta name="viewport"content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" 
+    integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">            
+</head>
+<body>
+    <header>
+        <h1>Hackathon : FAQ</h1>
+    </header>
+        
