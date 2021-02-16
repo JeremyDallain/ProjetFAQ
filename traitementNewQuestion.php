727c4310974e7b53974893d3b6561f8d24654f01
JeremyDallain <jeremy.dallain@gmail.com>
2021-02-16 15:41:09 +0100

    rajout du markdown

--- traitementNewQuestion.php
+++ traitementNewQuestion.php
@@ -9,10 +9,9 @@ $Parsedown = new Parsedown();
 
 if(isset($_POST['question']) && !empty($_POST['response'])){
     $question = $_POST['question'];
-    $response = $_POST['response'];
+    $response = $Parsedown->text($_POST['response']);
 
     $bdd=connexionPDO();
-
     $reponse = $bdd->prepare("INSERT INTO faq_cocktail (question, response, position) VALUES(:question, :response, 0)");
     $reponse->bindValue(':question', $question, PDO::PARAM_STR);
     $reponse->bindValue(':response', $response, PDO::PARAM_STR);
