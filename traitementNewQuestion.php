643a414795d6fe0d17fed7c6efa608969acecac3
JeremyDallain <jeremy.dallain@gmail.com>
2021-02-16 15:34:58 +0100

    insert

--- traitementNewQuestion.php
+++ traitementNewQuestion.php
@@ -1,14 +1,23 @@
 <?php
 
-
+require "vendor/autoload.php";
 require_once 'config/pdo.php';
-$bdd=connexionPDO();
+
+$Parsedown = new Parsedown();
+
+
 
 if(isset($_POST['question']) && !empty($_POST['response'])){
-$question = $_POST['question'];
-$response = $_POST['response'];
+    $question = $_POST['question'];
+    $response = $_POST['response'];
 
-$req = $bdd->query("INSERT INTO faq_cocktail(question,response,position) VALUES ('$question','$response',0)");
-$req->execute();
+    $bdd=connexionPDO();
 
+    $reponse = $bdd->prepare("INSERT INTO faq_cocktail (question, response, position) VALUES(:question, :response, 0)");
+    $reponse->bindValue(':question', $question, PDO::PARAM_STR);
+    $reponse->bindValue(':response', $response, PDO::PARAM_STR);
+    $reponse->execute();
+
+    header('Location: index.php');
+    exit();
 }
