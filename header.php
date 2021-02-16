935ada1bd994daa56819d6bf6b5a2512e9b5a5f0
AZEVsyl86 <azevedosylvain@gmail.com>
2021-02-16 15:38:58 +0100

    ajout favicon

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <title> Hackathon FAQ : <?php echo $titre; ?></title>
-    <link rel="icon" type="image/png" href="img/"/>
+    <link rel="icon" type="image/jpg" href="img/icone.jpg"/>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="styles/style.css"/>
     <meta name="viewport"content="width=device-width, initial-scale=1">
